516d93a383575fd86a01997339329467208a4f18
Aldo Souza <aldocnsouza@gmail.com>
2024-07-26 17:45:55 -0300

    config: global settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    title: 'Home | Portfolio'
+  },
+  {
+    path: "**",
+    pathMatch: "full",
+    redirectTo: ""
+  }
+];
