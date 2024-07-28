3932b6f9a6cc65e88322905c2ed4f0004a3dec72
Aldo Souza <aldocnsouza@gmail.com>
2024-07-28 15:13:43 -0300

    feat: add my projects section component

---
+++ src/app/page/components/my-projects/my-projects.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+import { Project } from '../../model/Project';
+import { ProjectCardComponent } from '../shared/cards/project-card/project-card.component';
+
+@Component({
+  selector: 'projects',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ProjectCardComponent
+  ],
+  templateUrl: './my-projects.component.html',
+  styleUrl: './my-projects.component.scss'
+})
+export class MyProjectsComponent {
+
+  theme: string = 'light'
+
+  projectList: Project[] = [
+    {
+      image: '/assets/project.jpg',
+      name: 'portifólio modelo',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad et accusantium natus ullam dolorem nisi, explicabo, eos similique aspernatur provident quisquam voluptate facilis hic, cupiditate debitis dignissimos a iste quod.',
+      languages: ["Java", "Spring Boot", "Angular", "Javascript", "Typescript", "MySQL", "PostgreSQL", "Jwt Token"],
+      linkDeploy: 'https://github.com/aldocsouza'
+    },
+    {
+      image: '/assets/project.jpg',
+      name: 'portifólio modelo',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad et accusantium natus ullam dolorem nisi, explicabo, eos similique aspernatur provident quisquam voluptate facilis hic, cupiditate debitis dignissimos a iste quod.',
+      languages: ["Java", "Spring Boot", "Angular", "Javascript", "Typescript", "MySQL", "PostgreSQL", "Jwt Token"],
+      linkDeploy: 'https://github.com/aldocsouza'
+    }
+  ]
+
+}
