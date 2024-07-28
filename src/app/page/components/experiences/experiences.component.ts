006aaa9e6794ffffc5bb5e4f103837ca26f0506e
Aldo Souza <aldocnsouza@gmail.com>
2024-07-28 15:12:25 -0300

    ref: decrease in code

--- src/app/page/components/experiences/experiences.component.ts
+++ src/app/page/components/experiences/experiences.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
-import { ExperienceCardComponent } from '../shared/experience-card/experience-card.component';
+import { ExperienceCardComponent } from '../shared/cards/experience-card/experience-card.component';
+import { Company } from '../../model/Company';
 
 @Component({
   selector: 'experiences',
@@ -14,5 +15,17 @@ import { ExperienceCardComponent } from '../shared/experience-card/experience-ca
   styleUrl: './experiences.component.scss'
 })
 export class ExperiencesComponent {
-theme: string = 'light'
+  theme: string = 'light'
+
+  experiencesLangs: Company[] = [
+    {
+      logo: '/assets/iasep.png',
+      period: 'Out 2023 - Presente',
+      name: 'iasep - instituto de assistência aos servidores do estado do pará',
+      role: 'estagiário desenvolvedor full stack',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad et accusantium natus ullam dolorem nisi, explicabo, eos similique aspernatur provident quisquam voluptate facilis hic, cupiditate debitis dignissimos a iste quod.',
+      languages: ["Java", "Spring Boot", "Angular", "Javascript", "Typescript", "MySQL", "PostgreSQL", "Jwt Token"]
+    }
+  ]
+
 }
