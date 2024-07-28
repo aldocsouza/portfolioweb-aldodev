dd8bbaea0effb7e6d9d4513d91c7e7ae75675435
Aldo Souza <aldocnsouza@gmail.com>
2024-07-28 15:15:05 -0300

    ref: button icon

--- src/app/page/components/shared/button/button.component.ts
+++ src/app/page/components/shared/button/button.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
 import { IconArrowRightComponent } from '../icons/icon-arrow-right/icon-arrow-right.component';
+import { IconArrowSquareComponent } from '../icons/icon-arrow-square/icon-arrow-square.component';
 
 @Component({
   selector: 'btn-primary',
   standalone: true,
   imports: [
     CommonModule,
-    IconArrowRightComponent
+    IconArrowRightComponent,
+    IconArrowSquareComponent
   ],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
@@ -16,5 +19,10 @@ export class ButtonComponent {
 
   @Input() btnStyle: string = 'btn';
   @Input() btnTitle: string = 'default'
+  @Output() buttonActive = new EventEmitter<boolean>(false);
+
+  onClick(){
+    this.buttonActive.emit(true)
+  }
 
 }
---
+++ src/app/page/components/shared/icons/icon-arrow-square/icon-arrow-square.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'icon-arrow-square',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon-arrow-square.component.html',
+})
+export class IconArrowSquareComponent {
+
+}
