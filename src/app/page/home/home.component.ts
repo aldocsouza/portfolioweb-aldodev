dde77c61bf40b487bc8647cac18b719899067411
Aldo Souza <aldocnsouza@gmail.com>
2024-07-29 21:35:52 -0300

    feat: add new component in home component

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -4,6 +4,10 @@ import { NavbarComponent } from '../components/navbar/navbar.component';
 import { HeroComponent } from '../components/hero/hero.component';
 import { AboutMeComponent } from "../components/about-me/about-me.component";
 import { TechnologiesComponent } from '../components/technologies/technologies.component';
+import { ExperiencesComponent } from '../components/experiences/experiences.component';
+import { MyProjectsComponent } from '../components/my-projects/my-projects.component';
+import { CertificationsComponent } from '../components/certifications/certifications.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
@@ -12,10 +16,14 @@ import { TechnologiesComponent } from '../components/technologies/technologies.c
     NavbarComponent,
     HeroComponent,
     AboutMeComponent,
-    TechnologiesComponent
+    TechnologiesComponent,
+    ExperiencesComponent,
+    MyProjectsComponent,
+    CertificationsComponent,
+    FooterComponent
 ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
 
