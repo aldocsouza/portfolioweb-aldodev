62d85e6d06114fc1474f0e82cb1ec1f74fed76a9
Aldo Souza <aldocnsouza@gmail.com>
2024-07-31 23:53:50 -0300

    ref: component style change

--- src/app/page/components/experiences/experiences.component.ts
+++ src/app/page/components/experiences/experiences.component.ts
@@ -3,13 +3,17 @@ import { Component } from '@angular/core';
 
 import { ExperienceCardComponent } from '../shared/cards/experience-card/experience-card.component';
 import { Company } from '../../model/Company';
+import { IconGitComponent } from '../shared/icons/icon-git/icon-git.component';
+import { IconInComponent } from '../shared/icons/icon-in/icon-in.component';
 
 @Component({
   selector: 'experiences',
   standalone: true,
   imports: [
     CommonModule,
-    ExperienceCardComponent
+    ExperienceCardComponent,
+    IconGitComponent,
+    IconInComponent
   ],
   templateUrl: './experiences.component.html',
   styleUrl: './experiences.component.scss'
@@ -19,10 +23,11 @@ export class ExperiencesComponent {
 
   experiencesLangs: Company[] = [
     {
+      recent: true,
       logo: '/assets/iasep.png',
       period: 'Out 2023 - Presente',
-      name: 'iasep - instituto de assistência aos servidores do estado do pará',
-      role: 'estagiário desenvolvedor full stack',
+      name: 'IASEP - Instituto de Assistência aos Servidores do Estado do Pará',
+      role: 'Estagiário Desenvolvedor Full Stack',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad et accusantium natus ullam dolorem nisi, explicabo, eos similique aspernatur provident quisquam voluptate facilis hic, cupiditate debitis dignissimos a iste quod.',
       languages: ["Java", "Spring Boot", "Angular", "Javascript", "Typescript", "MySQL", "PostgreSQL", "Jwt Token"]
     }
